a21139a19b048ad6a09e78f7a404e816d27bf7ad
martin muruthi <muruthimartin@gmail.com>
2019-01-30 22:07:39 +0300

    add code for pig latin

---
+++ pig_latin.py
@@ -0,0 +1,25 @@
+"""pig latin is an interesting python game. If a user inputa a word that atarts with
+a vowel, the program appends 'way' to the word. If the word starts with a consonant,
+the program removes the consonants preceeding the first vowel, appends the consonants
+to the end of the word and adds 'ay' at the end. Lets have fun with pig latin """
+word = input("Enter a word: ")
+vowels = ['a','e','i','o','u']
+def translate(word):
+    if len(word)>0 and word.isalpha():
+        word.lower()
+        if word[0] in vowels:
+            word = word+'way'
+            return word
+        else:
+            word.lower()
+            for letter in word:
+                if letter in vowels:
+                    vowel_index = word.index(letter)
+                    break
+            word = word[vowel_index:]+word[:vowel_index]+'ay'
+            return word
+    else:
+        print("Enter a correct word")
+print(translate(word))
+
+        
